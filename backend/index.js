bc33e1e9f158ec753855c17f935bb9f7b0f8de07
MADANA-GS <madangsnaik@gmail.com>
2025-09-01 00:09:34 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./utils/db/connectDb.js";
+import userRoutes from "./routes/user.routes.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -16,7 +17,8 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use("/api/users", userRoutes);
 app.listen(PORT, async () => {
-    await connectDB();
+  await connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
